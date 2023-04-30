76944cc2ed15aa6643052c4a124b3b3ea469f8c5
Jesús Enrique Rascón <blograso@gmail.com>
2023-04-30 12:21:26 +0200

    not working properly

--- src/lib/utils/utils.js
+++ src/lib/utils/utils.js
@@ -12,3 +12,43 @@ export function spin(node, options) {
 		}
 	};
 }
+
+export function getXHeight(font) {
+	const text = 'ax';
+	const canvas = document.querySelector('canvas');
+	canvas.width = 500;
+	canvas.height = 400;
+
+	const ctx = canvas.getContext('2d');
+	const fontSize = 100;
+
+	ctx.font = `${fontSize}px ${fontName}`;
+	// top is critical to the fillText() calculation
+	// you can use other positions, but you need to adjust the calculation
+
+	// ctx.textBaseline = 'top';
+	// ctx.textAlign = 'center';
+	const metrics = ctx.measureText(text);
+	const width = metrics.width;
+	const actualHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+	// fallback to using fontSize if fontBoundingBoxAscent isn't available, like in Firefox. Should be close enough that you aren't more than a pixel off in most cases.
+	const fontHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent ?? fontSize;
+	console.log(metrics.actualBoundingBoxAscent);
+
+	ctx.textAlign = 'left';
+	ctx.textBaseline = 'alphabetical';
+
+	ctx.font = `${fontSize}px ${fontName}`;
+	ctx.fillText('ax', width / 2, height / 2);
+	ctx.beginPath(); //
+	ctx.moveTo(0, height / 2);
+	ctx.lineTo(150, height / 2);
+	ctx.lineWidth = 1;
+	ctx.stroke();
+
+	ctx.beginPath(); //
+	ctx.moveTo(0, metrics.actualBoundingBoxAscent);
+	ctx.lineTo(150, metrics.actualBoundingBoxAscent);
+	ctx.lineWidth = 1;
+	ctx.stroke();
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,130 +1,128 @@
 <script>
-	import { fly } from 'svelte/transition';
+	import { fly, fade } from 'svelte/transition';
 	import { cubicOut } from 'svelte/easing';
 	import { ArrowCounterClockwise } from 'phosphor-svelte';
 
 	import { spin } from '$lib/utils/utils.js';
 	// import { fonts } from '$lib/fonts.js';
-	import { popularFonts } from '$lib/popularFonts.js';
+	import { fonts } from '$lib/fonts.js';
 	import { onMount } from 'svelte';
-
-	console.log(popularFonts.length);
 
 	let options = { duration: 300, easing: cubicOut };
 
-	let charSet = 'abcdefghijklmnopqrstuvwxyz';
+	let charSet = 'abcdefghijklmnopqrstuvwxyz1234567890.,:;-';
 	let charIndex = Math.floor(Math.random() * charSet.length);
-	let fontIndex = Math.floor(Math.random() * popularFonts.length);
+	let fontIndex = Math.floor(Math.random() * fonts.length);
 
 	$: selectedChar = charSet[charIndex];
-	$: selectedFont = popularFonts[fontIndex];
+	$: selectedFont = fonts[fontIndex];
 
 	let selectedCharKey = {};
-	let spinKey = {};
+	let spinReloadChar = {};
+	let spinReloadFont = {};
 
-	const reload = () => {
+	const reloadFont = () => {
 		charIndex = Math.floor(Math.random() * charSet.length);
-		fontIndex = Math.floor(Math.random() * popularFonts.length);
+		fontIndex = Math.floor(Math.random() * fonts.length);
 
 		selectedCharKey = {};
-		spinKey = {};
+		spinReloadFont = {};
+	};
+
+	const reloadChar = () => {
+		charIndex = Math.floor(Math.random() * charSet.length);
+
+		selectedCharKey = {};
+		spinReloadChar = {};
 	};
 
 	const keyReload = (e) => {
 		if (e.key === 'Enter' || e.key === 'r' || e.key === 'R') {
-			reload();
+			reloadChar();
+		}
+		if (e.key === 'Space' || e.key === 'f' || e.key === 'F') {
+			reloadFont();
 		}
 	};
 
-	$: fontName = selectedFont.family;
+	function createFontUrl(fontObject) {
+		let apiUrl = [];
+
+		apiUrl.push('https://fonts.googleapis.com/css2?family=');
+		apiUrl.push(fontObject.family.replace(/ /g, '+'));
+
+		if (fontObject.variants.length > 1) {
+			apiUrl.push(':wght@');
+
+			apiUrl.push(
+				fontObject.variants
+					.filter((v) => !v.match(/\D/g) || v === 'regular')
+					.join(';')
+					.replace('regular', '400')
+			);
+		}
+		apiUrl.push('&display=swap');
+		console.log(apiUrl.join(''));
+		return apiUrl.join('');
+	}
+
+	function loadFont(fontName, fontUrls) {
+		console.log(selectedFont);
+		for (const [key, value] of Object.entries(fontUrls)) {
+			if (!key.match(/\D/g) || key === 'regular') {
+				// console.log(key, value);
+				const font = new FontFace(fontName, `url(${value})`, { weight: key });
+				document.fonts.add(font);
+			}
+		}
+	}
+
 	$: fontUrls = selectedFont.files;
+	$: fontName = selectedFont.family;
 
 	let fontWeight = 700;
 
-	function getXHeight(font) {
-		const text = 'ax';
-		const canvas = document.querySelector('canvas');
-		canvas.width = 500;
-		canvas.height = 400;
-
-		const ctx = canvas.getContext('2d');
-		const fontSize = 100;
-
-		ctx.font = `${fontSize}px ${fontName}`;
-		// top is critical to the fillText() calculation
-		// you can use other positions, but you need to adjust the calculation
-		// ctx.textBaseline = 'top';
-		// ctx.textAlign = 'center';
-		const metrics = ctx.measureText(text);
-		const width = metrics.width;
-		const actualHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
-		// fallback to using fontSize if fontBoundingBoxAscent isn't available, like in Firefox. Should be close enough that you aren't more than a pixel off in most cases.
-		const fontHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent ?? fontSize;
-		console.log(metrics.actualBoundingBoxAscent);
-
-		ctx.textAlign = 'left';
-		ctx.textBaseline = 'alphabetical';
-
-		ctx.font = `${fontSize}px ${fontName}`;
-		ctx.fillText('ax', width / 2, height / 2);
-		ctx.beginPath(); //
-		ctx.moveTo(0, height / 2);
-		ctx.lineTo(150, height / 2);
-		ctx.lineWidth = 1;
-		ctx.stroke();
-
-		ctx.beginPath(); //
-		ctx.moveTo(0, metrics.actualBoundingBoxAscent);
-		ctx.lineTo(150, metrics.actualBoundingBoxAscent);
-		ctx.lineWidth = 1;
-		ctx.stroke();
-	}
+	let visible = false;
 
 	$: if (visible) {
-		console.log('mierdon');
-		for (const [key, value] of Object.entries(fontUrls)) {
-			if (key.match(regex) || key === 'regular') {
-				document.fonts.add(new FontFace(fontName, `url(${value})`, { weight: key }));
-			}
-		}
+		loadFont(fontName, fontUrls);
 	}
 
-	let visible = false;
-	const regex = /^\d{3}/g; //must only have 3 starting digits
 	onMount(() => {
-		for (const [key, value] of Object.entries(fontUrls)) {
-			if (key.match(regex) || key === 'regular') {
-				document.fonts.add(new FontFace(fontName, `url(${value})`, { weight: key }));
-			}
-		}
+		loadFont(fontName, fontUrls);
 		visible = true;
-		console.log('X height for ' + fontName + ': ' + getXHeight(fontName));
 	});
 </script>
 
 {#if visible}
-	<div in:fly={{ y: 100, duration: 1000 }} class="flex flex-col items-center">
+	<div in:fly={{ y: 100, duration: 1500 }} class="flex flex-col items-center">
 		{#key selectedCharKey}
 			<div
-				in:fly={{ y: 20 }}
-				class="glyph text-9xl max-w-sm mx-auto text-center my-20"
+				in:fly={{ y: 20, duration: 500 }}
+				class="glyph max-w-sm mx-auto text-center my-20"
 				style:font-family={fontName}
 				style:font-weight={fontWeight}
+				style:font-size={'200px'}
 			>
 				{selectedChar}
 			</div>
 		{/key}
 		<div>{fontName}</div>
 		<input type="range" min={100} max={900} bind:value={fontWeight} />
-		{#key spinKey}
-			<button on:click={reload} in:spin={options} class="text-3xl font-semibold"
-				><ArrowCounterClockwise weight="bold" size={40} /></button
-			>
-		{/key}
+		<div>
+			{#key spinReloadChar}
+				<button on:click={reloadChar} in:spin={options} class="text-3xl font-semibold"
+					><ArrowCounterClockwise weight="bold" size={40} /></button
+				>
+			{/key}
+			{#key spinReloadFont}
+				<button on:click={reloadFont} in:spin={options} class="text-3xl font-semibold"
+					><ArrowCounterClockwise weight="bold" size={40} /></button
+				>
+			{/key}
+		</div>
 	</div>
 {/if}
-
-<canvas class="border-2" id="fontCanvas" />
 
 <svelte:head>
 	<title>Font Guesser</title>
